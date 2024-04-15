cf022be36c460627cdc468a7cc63566a3169519e
cmc1026 <127949422+cmc1026@users.noreply.github.com>
2024-04-15 13:40:59 -0400

    Active link in NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,13 @@
+'use client';
 import React from 'react';
 import Link from 'next/link';
 import { FcTodoList } from "react-icons/fc";
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 
 
 const NavBar = () => {
+    const currentPath = usePathname();
     
     // In this example, the links variable is an array of objects 
     // where each object has a label property of type string 
@@ -11,7 +15,7 @@ const NavBar = () => {
     const links: { label: string, href: string }[] = [
         { label: 'Home', href: '/' },
         { label: 'Tasks', href: '/tasks' },
-        { label: 'New Task', href: '/newTask' },
+        { label: 'New Task', href: '/newtask' },
     ]; 
 
   return (
@@ -20,8 +24,14 @@ const NavBar = () => {
         <ul className="flex space-x-6">
             {links.map( link => 
                 <Link 
-                key={link.href} 
-                className='text-zinc-500 hover:text-blue-500 transision-colors' 
+                key={link.href}
+                // if link.href === currentPath, set the class to 'text-green-500', otherwise set it to 'text-zinc-500'
+                // className={`${link.href === currentPath ? 'text-green-500' : 'text-zinc-500'} text-zinc-500 hover:text-blue-500 transision-colors` }
+                className={classNames({
+                    'text-green-500': link.href === currentPath,
+                    'text-zinc-500': link.href !== currentPath,
+                    'hover:text-blue-500 transition-colors': true 
+                })}
                 href={ link.href }>{ link.label }</Link>
             )}
             
